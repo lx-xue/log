22ed4b2033def4efe2c548044ef0317ec30ad051
liuxue123456 <18749817085@163.com>
2021-02-01 14:51:46 +0800

    12

---
+++ .history/log_process_20210201101617.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Reader interface {
+	Read(rc chan []byte)
+}
+
+type Writer interface {
+	Write(wc chan *Message)
+}
+
+type LogProcess struct {
+	rc    chan []byte
+	wc    chan *Message
+	read  Reader
+	write Writer
+}
+
+//读取模块结构体
+type ReadFromFile struct {
+	path string //读取文件的路径
+}
+
+//写入模块结构体
+type WriteToInfluxDB struct {
+	influxDBDsn string //influx data source
+}
+
+type Message struct {
+	TimeLocal                    time.Time
+	BytesSent                    int
+	Path, Method, Scheme, Status string
+	UpstreamTime, RequestTime    float64
+}
+
+//读取方法
+func (r *ReadFromFile) Read(rc chan []byte) {
+	//读取模块
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file error:%s", err.Error()))
+	}
+
+	//从文件末尾开始逐行读取文件内容
+	f.Seek(0, 2) //字符指针移动到末尾
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadBytes('\n') //读取到换行符为止(就是读取一行内容)
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadBytes error:%s", err.Error()))
+		}
+
+		rc <- line[:len(line)-1] //去掉换行符
+	}
+
+}
+
+//写入方法
+func (*WriteToInfluxDB) Write(wc chan *Message) {
+	//写入模块
+	for v := range wc {
+		fmt.Println(v)
+	}
+
+}
+
+func (l *LogProcess) Process() {
+	//解析模板
+
+	r := regexp.MustCompile(`([\d\.]+)\s+([^ \[]+)\s+([^ \[]+)\s+\[([^\]]+)\]\s+([a-z]+)\s+\"([^"]+)\"\s+(\d{3})\s+(\d+)\s+\"([^"]+)\"\s+\"(.*?)\"\s+\"([\d\.-]+)\"\s+([\d\.-]+)\s+([\d\.-]+)`)
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	for v := range l.rc {
+		ret := r.FindStringSubmatch(string(v))
+		if len(ret) != 14 {
+			log.Println(len(ret))
+			log.Println(ret)
+			log.Println("FindStingSubmatch fail:", string(v))
+			continue
+		}
+
+		message := &Message{}
+
+		t, err := time.ParseInLocation("02/Jan/2006:15:04:05 +0000", ret[4], loc)
+		if err != nil {
+			log.Println("ParseInLocation fail", err.Error(), ret[4])
+		}
+		message.TimeLocal = t
+
+		byteSent, _ := strconv.Atoi(ret[8])
+		message.BytesSent = byteSent
+
+		//GET /foo?query=t HTTP?1.0
+		reqSli := strings.Split(ret[6], " ")
+		if len(reqSli) != 3 {
+			log.Println("strings.Split fail", ret[6])
+			continue
+		}
+		message.Method = reqSli[0]
+
+		u, err := url.Parse(reqSli[1])
+		if err != nil {
+			log.Println("url parse fail:", err)
+			continue
+		}
+
+		message.Path = u.Path
+		message.Scheme = ret[5]
+		message.Status = ret[7]
+
+		upstreamTime, _ := strconv.ParseFloat(ret[12], 64)
+		requestTime, _ := strconv.ParseFloat(ret[13], 64)
+
+		message.UpstreamTime = upstreamTime
+		message.RequestTime = requestTime
+
+		l.wc <- message
+	}
+}
+
+func main() {
+
+	r := &ReadFromFile{
+		path: "./access.log",
+	}
+
+	w := &WriteToInfluxDB{
+		influxDBDsn: "username&password..",
+	}
+	lp := &LogProcess{
+		rc:    make(chan []byte),
+		wc:    make(chan *Message),
+		read:  r,
+		write: w,
+	}
+	//Add(1)
+	//Done()
+	//Wait()
+	go lp.read.Read(lp.rc)
+	go lp.Process()
+	go lp.write.Write(lp.wc)
+	time.Sleep(300 * time.Second)
+}
